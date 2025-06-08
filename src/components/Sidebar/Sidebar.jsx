c1b39804d7a816891147776212c73aecd10eb63b
saigonga <saigonga34@gmail.com>
2025-06-08 21:39:21 +0530

    responsive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './Sidebar.css'
 
-import{
+import {
     HomeIcon,
     ChartBarIcon,
     DocumentTextIcon,
@@ -10,15 +10,13 @@ import{
     ClipboardDocumentListIcon
 } from '@heroicons/react/24/outline';
 
-
-function Sidebar () {
-
-    const navItems=[
-        {name: 'Dashboard', icon:HomeIcon, path:'/dashboard'},
-        {name: 'Analytics', icon:ChartBarIcon, path:'/analytics'},
-        {name: 'Activity Status', icon:DocumentTextIcon, path:'/activitystatus'},
-        {name: 'Calendar', icon:CalendarIcon, path:'/calendar'},
-        {name: 'Tasks', icon:ClipboardDocumentListIcon, path:'/tasks'}
+function Sidebar() {
+    const navItems = [
+        { name: 'Dashboard', icon: HomeIcon, path: '/dashboard' },
+        { name: 'Analytics', icon: ChartBarIcon, path: '/analytics' },
+        { name: 'Activity Status', icon: DocumentTextIcon, path: '/activitystatus' },
+        { name: 'Calendar', icon: CalendarIcon, path: '/calendar' },
+        { name: 'Tasks', icon: ClipboardDocumentListIcon, path: '/tasks' }
     ];
 
     return (
@@ -28,18 +26,18 @@ function Sidebar () {
             </div>
             <nav className="nav-menu">
                 <ul>
-                    {navItems.map((item)=>(     
+                    {navItems.map((item) => (
                         <li key={item.name}>
                             <Link to={item.path} className="nav-link">
-                            <item.icon className="nav-icon"/>
-                            <span>{item.name}</span>
+                                <item.icon className="nav-icon" />
+                                <span>{item.name}</span>
                             </Link>
-                        </li> 
+                        </li>
                     ))}
                 </ul>
             </nav>
         </div>
-    )
+    );
 }
 
 export default Sidebar;
